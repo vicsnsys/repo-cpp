12e231867a3b22ed114380c4c9c787d82427ccb0
viccsn <victorsoaressn@gmail.com>
2025-05-08 17:18:43 -0300

    add: 4th exc (F)

---
+++ LAB-11/LAB-11/3F.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+	char nomeC[5];
+	std::string nomeS;
+
+	std::cout << "C: " << nomeC << std::endl;
+	std::cout << "S: " << nomeS << std::endl;
+
+	return 0;
+}
---
+++ LAB-11/LAB-11/4F.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+	char nome[] = "C++ Primer Plus";
+
+	std::cout << strlen(nome) << std::endl;
+	std::cout << sizeof(nome);
+
+	return 0;
+}
