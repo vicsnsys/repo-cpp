297cc311a5155a7917aa4b24ef81bcd04b1cf143
vicsnsys <victorsoaressn@gmail.com>
2025-05-19 16:11:34 -0300

    add: 3rd exc (R) [using ostream n pointer to access my own type]

---
+++ LAB-14/LAB-14/3R.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+struct peixe {
+	std::string tipo;
+	float peso = 0;
+	int comprimento = 0;
+};
+
+std::ostream& operator<<(std::ostream& os, peixe temp);
+
+int main() {
+
+	peixe p1 = {};
+	peixe* ptr = &p1;
+
+	ptr->tipo = "Dourado";
+	ptr->peso = 4.5f;
+	ptr->comprimento = 60;
+
+	std::cout << p1 << std::endl;
+
+	return 0;
+}
+
+std::ostream& operator<<(std::ostream& os, peixe temp) {
+	os << temp.tipo << "\n";
+	os << temp.peso << "\n";
+	os << temp.comprimento<< "\n";
+	return os;
+}
